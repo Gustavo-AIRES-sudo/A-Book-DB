3cf3c616d95f4029b6407daba0ebe1f1915d6a1a
Gustavo <cesarfilhoandrade8@gmail.com>
2026-01-18 22:52:37 -0300

    refactor: includes the annotation @ToString in the models of User and the Books.

--- src/main/java/Book/demo/Books/BooksModel.java
+++ src/main/java/Book/demo/Books/BooksModel.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
@@ -14,6 +15,7 @@ import org.hibernate.annotations.DynamicUpdate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@ToString(exclude = "userModel")
 
 public class BooksModel {
     @Id
--- src/main/java/Book/demo/User/UserModel.java
+++ src/main/java/Book/demo/User/UserModel.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.DynamicUpdate;
 
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@ToString
 
 public class UserModel {
 
